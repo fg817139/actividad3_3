252ec33b91244f93eeef3cee92c56f094d184da3
andres felipe <agonzalez104@soyudemedellin.edu.co>
2023-02-14 13:10:44 -0500

    solucion al ejercicio 7, cuenta bancaria

---
+++ ejercicios/ejercicio7.py
@@ -0,0 +1,5 @@
+class CuentaBancaria:
+    def __init__(self, numero_cuenta, propietarios, balance):
+        self.numero_cuenta = numero_cuenta
+        self.propietarios = propietarios
+        self.balance = balance
