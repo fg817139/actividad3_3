f7afc24f5f33c4cdfe965b5228ba9b74eb24cbcc
andres felipe <agonzalez104@soyudemedellin.edu.co>
2023-02-14 13:22:27 -0500

    solucion al ejercicio 7 y 8, union de los ejercicios aplicando las soluciones

--- ejercicios/ejercicio7.py
+++
@@ -1,5 +0,0 @@
-class CuentaBancaria:
-    def __init__(self, numero_cuenta, propietarios, balance):
-        self.numero_cuenta = numero_cuenta
-        self.propietarios = propietarios
-        self.balance = balance
